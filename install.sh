a7f319d835356dfd44451e31eb1118fcc7708f73
David Brusius <brusiusdavid@gmail.com>
2023-10-11 14:12:56 -0300

    Add option to run specific installers only

--- install.sh
+++ install.sh
@@ -10,9 +10,27 @@ source ${DOTFILES_INSTALLER}/custom_env_vars
 
 print_in_purple 'Running installers'
 
-${DOTFILES_INSTALLER}/brew
-${DOTFILES_INSTALLER}/git
-${DOTFILES_INSTALLER}/osx
-${DOTFILES_INSTALLER}/shell
+ask_options "installers_to_run" \
+  "Installers to run" \
+  "all" "brew" "git" "osx" "shell"
 
-print_in_green '\nSuccess!\n' && exit 0
+print_in_green "Running '$installers_to_run' installer(s)"
+
+if [[ $installers_to_run == "brew" || $installers_to_run == "all" ]]; then
+  ${DOTFILES_INSTALLER}/brew
+fi
+
+if [[ $installers_to_run == "git" || $installers_to_run == "all" ]]; then
+  ${DOTFILES_INSTALLER}/git
+fi
+
+if [[ $installers_to_run == "osx" || $installers_to_run == "all" ]]; then
+  ${DOTFILES_INSTALLER}/osx
+fi
+
+if [[ $installers_to_run == "shell" || $installers_to_run == "all" ]]; then
+  ${DOTFILES_INSTALLER}/shell
+fi
+
+
+print_in_green "Success!" && exit 0
